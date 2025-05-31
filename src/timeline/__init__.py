af5750e296e14c68341f5d491f8d257e3c870ede
nicos_backbase <nicos@backbase.com>
2025-05-31 22:41:08 +0200

    chore: add project script

--- src/timeline/__init__.py
+++ src/timeline/__init__.py
@@ -4,10 +4,12 @@ from .timeline import (
     extract_transition_dates,
     display_results,
 )
+from .main_cli import main
 
 __all__ = [
     'connect_to_jira',
     'calculate_epic_timeline',
     'extract_transition_dates',
     'display_results',
+    'main',
 ]
